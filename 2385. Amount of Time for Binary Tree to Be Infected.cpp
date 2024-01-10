f0e8a93c265317a5cc66fceeb584531cb5234ee5
Durgesh Raj <142685180+er-durgeshRaj@users.noreply.github.com>
2024-01-10 21:33:59 +0530

    Create 2385. Amount of Time for Binary Tree to Be Infected.cpp

---
+++ 2385. Amount of Time for Binary Tree to Be Infected.cpp
@@ -0,0 +1,90 @@
+// QUESTION
+// You are given the root of a binary tree with unique values, and an integer start. At minute 0, an infection starts from the node with value start.
+
+// Each minute, a node becomes infected if:
+
+// The node is currently uninfected.
+// The node is adjacent to an infected node.
+// Return the number of minutes needed for the entire tree to be infected.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,5,3,null,4,10,6,9,2], start = 3
+// Output: 4
+// Explanation: The following nodes are infected during:
+// - Minute 0: Node 3
+// - Minute 1: Nodes 1, 10 and 6
+// - Minute 2: Node 5
+// - Minute 3: Node 4
+// - Minute 4: Nodes 9 and 2
+// It takes 4 minutes for the whole tree to be infected so we return 4.
+// Example 2:
+
+
+// Input: root = [1], start = 1
+// Output: 0
+// Explanation: At minute 0, the only node in the tree is infected so we return 0.
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 105].
+// 1 <= Node.val <= 105
+// Each node has a unique value.
+// A node with a value of start exists in the tree.
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SOLUTION
+class Solution {
+public:
+    void convert(TreeNode* current, int parent, unordered_map<int, vector<int>>& adj) {
+        if (current == nullptr) {
+            return;
+        }
+
+        if (parent != -1) {
+            adj[current->val].push_back(parent);
+        }
+
+        if (current->left != nullptr) {
+            adj[current->val].push_back(current->left->val);
+        } 
+        if (current->right != nullptr) {
+            adj[current->val].push_back(current->right->val);
+        }
+        convert(current->left, current->val, adj);
+        convert(current->right, current->val, adj);
+    }
+
+    int amountOfTime(TreeNode* root, int start) {
+        unordered_map<int, vector<int>> adj;
+        convert(root, -1, adj);
+
+        queue<int> que;
+        que.push(start);
+        unordered_set<int> visited;
+        visited.insert(start);
+        int minutes = 0;
+
+        while (!que.empty()) {
+            int n = que.size();
+
+            while (n--) {
+                int curr = que.front();
+                que.pop();
+
+                for (int &ngbr : adj[curr]) {
+                    if (visited.find(ngbr) == visited.end()) {
+                        que.push(ngbr);
+                        visited.insert(ngbr);
+                    }
+                }
+            }
+            minutes++;
+        }
+
+        return minutes - 1;
+    }
+};
