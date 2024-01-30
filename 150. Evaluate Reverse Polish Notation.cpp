0721139eaa054bc998e805fb77952b56d981ed81
Durgesh Raj <142685180+er-durgeshRaj@users.noreply.github.com>
2024-01-30 22:37:57 +0530

    Create 150. Evaluate Reverse Polish Notation.cpp

---
+++ 150. Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,80 @@
+// QUESTION
+// You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+// Evaluate the expression. Return an integer that represents the value of the expression.
+
+// Note that:
+
+// The valid operators are '+', '-', '*', and '/'.
+// Each operand may be an integer or another expression.
+// The division between two integers always truncates toward zero.
+// There will not be any division by zero.
+// The input represents a valid arithmetic expression in a reverse polish notation.
+// The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+
+// Example 1:
+
+// Input: tokens = ["2","1","+","3","*"]
+// Output: 9
+// Explanation: ((2 + 1) * 3) = 9
+// Example 2:
+
+// Input: tokens = ["4","13","5","/","+"]
+// Output: 6
+// Explanation: (4 + (13 / 5)) = 6
+// Example 3:
+
+// Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+// Output: 22
+// Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+// = ((10 * (6 / (12 * -11))) + 17) + 5
+// = ((10 * (6 / -132)) + 17) + 5
+// = ((10 * 0) + 17) + 5
+// = (0 + 17) + 5
+// = 17 + 5
+// = 22
+ 
+
+// Constraints:
+
+// 1 <= tokens.length <= 104
+// tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SOLUTION
+class Solution {
+public:
+    int operate(int &a, int &b, const string &s) {
+        if(s == "+")
+            return a+b;
+        
+        if(s == "-")
+            return a-b;
+        
+        if(s == "*")
+            return (long)a*(long)b;
+        
+        if(s == "/")
+            return a/b;
+        
+        return -1;
+    }
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        int result = 0;
+        for(const string& s:tokens) {
+            if(s == "+" || s == "-" || s == "*" || s == "/") {
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
+                
+                result = operate(a, b, s);
+                st.push(result);
+            } else {
+                st.push(stoi(s));
+            }
+        }
+        return st.top();
+    }
+};
