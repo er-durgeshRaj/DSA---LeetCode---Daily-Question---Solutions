a0cf4c19ddf8dea0dd77fa4b5ee37e3eeeabaa9e
Durgesh Raj <142685180+er-durgeshRaj@users.noreply.github.com>
2024-03-21 23:38:09 +0530

    Update 206. Reverse Linked List.cpp

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -25,51 +25,25 @@
 // -5000 <= Node.val <= 5000
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // SOLUTION
-class Solution
-{
-    public:
-    
-    struct Node* mergeTwoLists(Node *list1,Node *list2) {
-        if(list1 == NULL) {
-            return list2;
-        }
-
-        if(list2 == NULL) {
-            return list1;
-        }
-
-        struct Node* result;
-        if(list1->data < list2->data) {
-            result = list1;
-            result->next = mergeTwoLists(list1->next, list2);
-        } else {
-            result = list2;
-            result->next = mergeTwoLists(list1, list2->next);
-        }
-
-        return result;
-    }
-    
-    struct Node* reverseList(struct Node* head) {
-        if(head == NULL || head->next == NULL) {
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) 
+    {
+        if(head == NULL || head->next == NULL) // [] or [1]
             return head;
+        
+        ListNode* current = head;
+        ListNode* prev = NULL;
+        ListNode* nxt = head->next;
+        
+        // O(n)
+        while(current != NULL)
+        {
+            current->next = prev;
+            prev = current;
+            current = nxt;
+            if(nxt != NULL) nxt = nxt->next;
         }
-
-        struct Node* last = reverseList(head->next); //last = future head
-
-        head->next->next = head;
-        head->next = NULL;
-
-        return last;
+       return prev; 
     }
-    
-    struct Node * mergeResult(Node *node1,Node *node2) {
-        struct Node* result = mergeTwoLists(node1, node2);
-        
-        
-        result = reverseList(result);
-        
-        return result;
-        
-    }  
 };
